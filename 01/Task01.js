34f7577482a0e1869b1b91b8f74b8212ee09edbb
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 14:48:14 +0100

    task 1

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,46 +1,56 @@
 import React, { useState } from 'react';
 import Subject from './../src/libs/Subject';
 
+
+const subject = new Subject();
+
 const Task01 = () => {
     const [time, setTime] = useState(0);
     const [tagName, setTagName] = useState('??');
     const [cursorPosition, setCursorPosition] = useState('[?,?]');
 
-    const setEventTime = ({timeStamp}) => {
-        setTime( parseInt(timeStamp / 1000) );
-    }
+    const setEventTime = ({ timeStamp }) => {
+        setTime(parseInt(timeStamp / 1000));
+    };
 
-    const setEventTagName = ({target}) => {
+    const setEventTagName = ({ target }) => {
         setTagName(target.tagName);
-    }
+    };
 
-    const setEventCursorPosition = ({pageX, pageY}) => {
+    const setEventCursorPosition = ({ pageX, pageY }) => {
         setCursorPosition(`[${pageX},${pageY}]`);
-    }
+    };
+
+    const handleEvent = ({ timeStamp, target, pageX, pageY }) => {
+        setTime(parseInt(timeStamp / 1000));
+        setTagName(target.tagName);
+        setCursorPosition(`[${pageX},${pageY}]`);
+    };
+
+    subject.subscribe(handleEvent);
 
     return (
         <section>
             <h1>Task 1</h1>
-            
-            <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
-            }}>
+
+            <div
+                onClick={(event) => {
+                    subject.notify(event);
+                }}
+            >
                 <p>
-                    <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
+                    <strong>Kliknij wybrany element:</strong> <a>link</a>,{' '}
+                    <button>button</button>, <span>span</span>
                 </p>
 
-
                 <ul>
-                    <li>time: { time }s</li>
-                    <li>tagName: { tagName }</li>
-                    <li>position: { cursorPosition }</li>
+                    <li>time: {time}s</li>
+                    <li>tagName: {tagName}</li>
+                    <li>position: {cursorPosition}</li>
                 </ul>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Task01;
-
