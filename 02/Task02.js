f3405e783392a2377f51b75597596a0afac25dae
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 14:48:55 +0100

    task 2

--- 02/Task02.js
+++ 02/Task02.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { useStore, useSelector } from 'react-redux'; 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const Task02 = () => {
+import Task01 from './../../01/Task01';
+import Task02 from './../../02/Task02';
+import Task03 from './../../03/Task03';
+import Task04 from './../../04/Task04';
+import Task05 from './../../05/Task05';
 
-    const getMessage = ({message = 'Magazyn nie został zaimplementowany prawidłowo!'}) => {
-        return message;
-    }
-
-    const message = useSelector(getMessage);
-
+const store = createStore(() => {
+    return { message: 'Działa!' };
+});
+const App = () => {
     return (
-        <section>
-            <h1>Task 2</h1>
-            
-            <div>{ message }</div>
-        </section>
+        <Provider store={store}>
+            <Task01 />
+            <Task02 />
+            {/* <Task03 /> */}
+            {/* <Task04 /> */}
+            {/* <Task05 /> */}
+        </Provider>
     );
-}
+};
 
-export default Task02;
-
+export default App;
