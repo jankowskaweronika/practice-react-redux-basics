46666121e2924c20b12db5e8feaa8db810eab657
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 14:51:03 +0100

    task 4

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import UserList from './../src/components/UserList';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Task01 from './../../01/Task01';
+import Task02 from './../../02/Task02';
+import Task03 from './../../03/Task03';
+import Task04 from './../../04/Task04';
+import Task05 from './../../05/Task05';
 
-const Task04 = () => {
+
+const initialState = { message: 'Działa!', time: new Date(), users: [] };
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'getCurrentTime':
+            return { ...state, time: new Date() };
+        case 'addUser':
+            const { data } = action.payload;
+            return { ...state, users: [...state.users, data] };
+        case 'removeUser': {
+            const { id } = action.payload;
+            return {
+                ...state,
+                users: state.users.filter((user) => user.id !== id),
+            };
+        }
+    }
+    return state;
+};
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+const App = () => {
     return (
-        <section>
-            <h1>Task 4</h1>
-
-            <UserList />
-        </section>
+        <Provider store={store}>
+            <Task01 />
+            <Task02 />
+            <Task03 />
+            <Task04 />
+            {/* <Task05 /> */}
+        </Provider>
     );
-}
-
-export default Task04;
-
+};
+export default App;
---
+++ 04/components/UserItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+function removeUser(id) {
+  return {
+    type: 'removeUser',
+    payload: {
+      id,
+    },
+  };
+}
+
+const UserItem = ({ name = 'Default Name', id = -1 }) => {
+  const dispatch = useDispatch();
+  return (
+    <li>
+      {name} <button onClick={() => dispatch(removeUser(id))}>usuń</button>
+    </li>
+  );
+};
